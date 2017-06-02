9de36c9b264dd98eb99ecc3ecc2987a07eba673a
Jasmin <j.aumann1996@gmx.de>
2017-06-02 08:38:48 +0200

    Add tabs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 
 import javax.sound.midi.Soundbank;
@@ -21,62 +22,15 @@ public class Controller implements Initializable {
     public TextField name;
     public TextField adresse;
     public TextField telefonnummer;
-    public Integer Namennummer;
-    public String dateiNamennummer;
     public TabPane tabs;
     public Button suchen;
     public Button speichern;
-
-
-    public int setNamennummer(String name) throws IOException {
-        String text = "";
-        byte zeichen;
-        int dateiNN = 0;
-
-        String dateiName = name + ".txt";
-        FileOutputStream speichern = new FileOutputStream(dateiName);
-        speichern.close();
-
-        String dateiNamennummer = Namennummer + ".txt";
-        FileInputStream suche = new FileInputStream(dateiNamennummer);
-        do {
-            zeichen = (byte) suche.read();
-            text += (char) zeichen;
-        } while (zeichen != -1);
-
-        text = dateiNamennummer;
-
-        if (Integer.parseInt(dateiNamennummer) != 0) {
-            dateiNN = Integer.parseInt(dateiNamennummer + 1);
-            speichern.write((byte) dateiNN);
-            speichern.close();
-
-            return dateiNN;
-        }
-        dateiNN += 1;
-        speichern.write((byte) dateiNN);
-        speichern.close();
-
-        return dateiNN;
-    }
-
-    ;
-
-    public int getNamennummer(String name) throws IOException {
-        String text = "";
-        byte zeichen;
-
-        String dateiName = name + ".txt";
-        FileInputStream suchen = new FileInputStream(dateiName);
-        do {
-            zeichen = (byte) suchen.read();
-            text += (char) zeichen;
-        } while (zeichen != -1); // bis zum Ende
-
-        text = Integer.toString(Namennummer);
-
-        return Namennummer;
-    }
+    public Label labAdresse;
+    public Label labTelefon;
+    public Button startNeu;
+    public Button startSuche;
+    public Boolean tabNeu = false;
+    public Button navi;
 
 
     public void changeColor(DragEvent dragEvent) {
@@ -95,9 +49,7 @@ public class Controller implements Initializable {
         String strTelefonnummer = telefonnummer.getText();
 
 
-        int sNN = 1; //setNamennummer(strName);
-
-        String dateiName = sNN + "Person" + strName + ".txt";
+        String dateiName = "Person" + strName + ".txt";
 
         FileOutputStream speichern = new FileOutputStream(dateiName);
         for (int i = 0; i < strName.length(); i++) {
@@ -105,7 +57,7 @@ public class Controller implements Initializable {
         }
         speichern.close();
 
-        String dateiAdresse = sNN + "Person" + strName + 1 + ".txt";
+        String dateiAdresse = "Person" + strName + 1 + ".txt";
         FileOutputStream speichern1 = new FileOutputStream(dateiAdresse);
         for (int i = 0; i < strAdresse.length(); i++) {
             speichern1.write((byte) strAdresse.charAt(i));
@@ -113,7 +65,7 @@ public class Controller implements Initializable {
         speichern1.close();
 
 
-        String dateiTelefonnummer = sNN + "Person" + strName + 2 + ".txt";
+        String dateiTelefonnummer = "Person" + strName + 2 + ".txt";
         FileOutputStream speichern2 = new FileOutputStream(dateiTelefonnummer);
         for (int i = 0; i < strTelefonnummer.length(); i++) {
             speichern2.write((byte) strTelefonnummer.charAt(i));
@@ -129,55 +81,118 @@ public class Controller implements Initializable {
         char buchstabe;
         String text = "";
         String text2 = "";
-        Integer gNN = 1;//getNamennummer(name.getText()) ;
-        
-       /* String dateiName = gNN + "Person" + name + ".txt";
-        FileInputStream suchen = new FileInputStream(dateiName);
-        do{
-            zeichen = (byte) suchen.read();
-            text += (char)zeichen;
-        } while (zeichen != -1); // bis zum Ende
 
-        text = name;
-          suchen.close();*/
-
-        String dateiName1 = gNN + "Person" + name.getText() + 1 + ".txt";
+        String dateiName1 = "Person" + name.getText() + 1 + ".txt";
         FileInputStream suchen1 = new FileInputStream(dateiName1);
         do {
             zeichen = (byte) suchen1.read();
             text += (char) zeichen;
         } while (zeichen != -1); // bis zum Ende
 
-        adresse.setText(text);
+        labAdresse.setText(text);
 
         suchen1.close();
 
-        String dateiName2 = gNN + "Person" + name.getText() + 2 + ".txt";
+        String dateiName2 = "Person" + name.getText() + 2 + ".txt";
         FileInputStream suchen2 = new FileInputStream(dateiName2);
         do {
             zeichen = (byte) suchen2.read();
             text2 += (char) zeichen;
         } while (zeichen != -1); // bis zum Ende
 
-        telefonnummer.setText(text2);
+        labTelefon.setText(text2);
 
         suchen2.close();
+
+        suchen.setVisible(false);
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tabs.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> {
+                (ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) -> {
                     System.out.println("OLD: " + oldValue.getText());
                     System.out.println("NEW: " + newValue.getText());
                     //setvisible
-                    if(newValue.getText().equals("2")) {
-                    suchen.setVisible(true);
-                    speichern.setVisible(false);
+                    if(newValue.getText().equals("Suchen")) {
+                        labAdresse.setVisible(true);
+                        labTelefon.setVisible(true);
+                        startNeu.setVisible(false);
+                        startSuche.setVisible(false);
+                        name.setVisible(true);
+
+                        suchen.setVisible(true);
+                        speichern.setVisible(false);
+                        adresse.setVisible(false);
+                        telefonnummer.setVisible(false);
+                        navi.setVisible(true);
+                    }
+                    if(newValue.getText().equals("Speichern")) {
+                        labAdresse.setVisible(false);
+                        labTelefon.setVisible(false);
+                        startNeu.setVisible(false);
+                        startSuche.setVisible(false);
+                        name.setVisible(true);
+
+                        suchen.setVisible(false);
+                        speichern.setVisible(true);
+                        adresse.setVisible(true);
+                        telefonnummer.setVisible(true);
+                        navi.setVisible(false);
+                    }
+                    if(newValue.getText().equals("Start")) {
+                        labAdresse.setVisible(false);
+                        labTelefon.setVisible(false);
+                        suchen.setVisible(false);
+                        speichern.setVisible(false);
+                        adresse.setVisible(false);
+                        telefonnummer.setVisible(false);
+                        name.setVisible(false);
+                        navi.setVisible(false);
+
+                        startNeu.setVisible(true);
+                        startSuche.setVisible(true);
+                    }
+
+                    if(tabNeu){
+                        newValue.setText("Navigation");
+                        labAdresse.setVisible(false);
+                        labTelefon.setVisible(false);
+                        suchen.setVisible(false);
+                        speichern.setVisible(false);
+                        adresse.setVisible(false);
+                        telefonnummer.setVisible(false);
+                        name.setVisible(false);
+
+                        startNeu.setVisible(false);
+                        startSuche.setVisible(false);
+                        navi.setVisible(false);
+
 
                     }
-                }
+    }
         );
     }
+
+    public void tabNeuKontakt(MouseDragEvent mouseDragEvent) {
+    }
+
+    public void tabKontaktSuche(MouseDragEvent mouseDragEvent) {
+    }
+
+    public void tabMaps(MouseDragEvent mouseDragEvent) {
+       /* tabNeu = true;
+        initialize(URL location, ResourceBundle resource); */
+    }
+
+
+  /*  public boolean tabKontaktSuche(MouseEvent) throws IOException {
+       if();)
+           return true;
+    } return false;
+
+    public void tabNeuKontakt(MouseDragEvent mouseDragEvent) {
+
+    } */
 }
